a3b3822b2d981e4673ea76143255177849e746a6
Sophie <sophieeqin@gmail.com>
2020-05-20 05:14:36 -0500

    build + configs to deploy

---
+++ build/precache-manifest.06f073231f5e510bbbfaff07a6990688.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ae72a2a5d4334470c12713b59ebf19",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "1e683709f79c592418e6",
+    "url": "/personal-website/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "5dc3e08df30b1f98100d",
+    "url": "/personal-website/static/css/main.c4708798.chunk.css"
+  },
+  {
+    "revision": "1e683709f79c592418e6",
+    "url": "/personal-website/static/js/2.a12ef77e.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/personal-website/static/js/2.a12ef77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc3e08df30b1f98100d",
+    "url": "/personal-website/static/js/main.8b3114e6.chunk.js"
+  },
+  {
+    "revision": "2e7cb654a01048761c63",
+    "url": "/personal-website/static/js/runtime-main.59ed5271.js"
+  },
+  {
+    "revision": "e02041bcd5355b3d38da5f2edaef4f2e",
+    "url": "/personal-website/static/media/Sophie_Qin_Resume.e02041bc.pdf"
+  },
+  {
+    "revision": "901b7de6674fd0c116fb9bf4f24ab4f6",
+    "url": "/personal-website/static/media/cc3k_product.901b7de6.png"
+  },
+  {
+    "revision": "864aed39a75fb3aa52d3f478a4f4c9d0",
+    "url": "/personal-website/static/media/dancefest_product_transparent.864aed39.png"
+  },
+  {
+    "revision": "df8858a502cf01de75372bc05dd2d3a4",
+    "url": "/personal-website/static/media/mimelons_product.df8858a5.png"
+  },
+  {
+    "revision": "61698a1c7708e9d854dce36e3c08032a",
+    "url": "/personal-website/static/media/sophie-homepage.61698a1c.png"
+  },
+  {
+    "revision": "36d022a27fbd2212ff5b46871ea642b9",
+    "url": "/personal-website/static/media/sophie_dp.36d022a2.png"
+  },
+  {
+    "revision": "8b839128ec20bfa03a15158618c69a95",
+    "url": "/personal-website/static/media/website.8b839128.png"
+  }
+]);
