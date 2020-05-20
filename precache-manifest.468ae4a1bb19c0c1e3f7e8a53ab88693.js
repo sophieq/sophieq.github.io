47ff1c6ec1da2b85a812b9272eb219e4816a7237
Sophie <sophieeqin@gmail.com>
2020-05-20 06:01:25 -0500

    Updates

---
+++ precache-manifest.468ae4a1bb19c0c1e3f7e8a53ab88693.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c7d3fb0ee9084341c73dd4c03639bf",
+    "url": "/narcissus/index.html"
+  },
+  {
+    "revision": "1e683709f79c592418e6",
+    "url": "/narcissus/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ab36a374e7ec18c8c3c3",
+    "url": "/narcissus/static/css/main.c4708798.chunk.css"
+  },
+  {
+    "revision": "1e683709f79c592418e6",
+    "url": "/narcissus/static/js/2.a12ef77e.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/narcissus/static/js/2.a12ef77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab36a374e7ec18c8c3c3",
+    "url": "/narcissus/static/js/main.a0615828.chunk.js"
+  },
+  {
+    "revision": "84be04b10fdf83099f70",
+    "url": "/narcissus/static/js/runtime-main.bbf739af.js"
+  },
+  {
+    "revision": "e02041bcd5355b3d38da5f2edaef4f2e",
+    "url": "/narcissus/static/media/Sophie_Qin_Resume.e02041bc.pdf"
+  },
+  {
+    "revision": "901b7de6674fd0c116fb9bf4f24ab4f6",
+    "url": "/narcissus/static/media/cc3k_product.901b7de6.png"
+  },
+  {
+    "revision": "864aed39a75fb3aa52d3f478a4f4c9d0",
+    "url": "/narcissus/static/media/dancefest_product_transparent.864aed39.png"
+  },
+  {
+    "revision": "df8858a502cf01de75372bc05dd2d3a4",
+    "url": "/narcissus/static/media/mimelons_product.df8858a5.png"
+  },
+  {
+    "revision": "61698a1c7708e9d854dce36e3c08032a",
+    "url": "/narcissus/static/media/sophie-homepage.61698a1c.png"
+  },
+  {
+    "revision": "36d022a27fbd2212ff5b46871ea642b9",
+    "url": "/narcissus/static/media/sophie_dp.36d022a2.png"
+  },
+  {
+    "revision": "8b839128ec20bfa03a15158618c69a95",
+    "url": "/narcissus/static/media/website.8b839128.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/narcissus/precache-manifest.468ae4a1bb19c0c1e3f7e8a53ab88693.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/narcissus/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
